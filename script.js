f58e5ba359ac92676773339e5e8b00ea62614835
ChanChanEz <chandrawinterwar@gmail.com>
2021-10-18 18:37:28 +0700

    Forgot to edit in github folder

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("div.anchors").fadeIn(1500);
+    $("div.about").fadeIn(1500);
+});
+
+// $(document).on(scroll, function(){
+//     $(window).scroll( function(){
+//         $("div.about2").fadeIn(1500);
+//     });
+// });
