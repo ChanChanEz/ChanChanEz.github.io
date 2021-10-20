6b450e9d061aa53309d72a00497e8633839b1781
ChanChanEz <chandrawinterwar@gmail.com>
2021-10-20 19:52:19 +0700

    Ganti design
    ganti design,
    TODO:
    -. isi foto
    -. isi about
    
    --NEW CLASS: contacts--
    -. isiin sosmed
    -. button linkedin
    
    --NEW CLASS: Portofolio--
    -. Masukin (gk tau apa)

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 $(document).ready(function(){
-    $("div.title").fadeIn(1300);
-    $("div.anchors").fadeIn(1500);
-    $("div.about").fadeIn(1700);
+    // $("div.title").fadeIn(1300);
+    $("div.anchors").fadeIn(1000);
+    $("div.content").fadeIn(1700);
     
+});
+
+// var h = $('.anchors').height();
+// document.getElementById('.about').style.marginTop= 100;
+
+jQuery(document).ready(function($){ 
+    $('.about').css({
+        'margin-top' : $('.anchors').height() * 1.2
+    });
 });
 
 // $(document).on(scroll, function(){
 //     $(window).scroll( function(){
 //         $("div.about2").fadeIn(1500);
+//     });
+// });
+
+// jQuery(document).ready(function($){ 
+//     $('.about').each(function(){ 
+//         $(this).css({
+//             'margin-top' : $('.anchors').height() * 1.2
+//         });
 //     });
 // });
